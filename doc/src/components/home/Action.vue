b442cbf84dc4e7c27e3fb48f10e4315315501b1a
Yanze Dai <yanze.dai@rakuten.com>
2020-05-02 22:36:57 +0900

    [doc] chore: optimize home page layout

--- doc/src/components/home/Action.vue
+++ doc/src/components/home/Action.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="action">
-    <h3 class="font-bold">Shall We Move on?</h3>
+    <h1>Shall We Move on?</h1>
+
     <p class="my-6">Take a few more steps to build up your own components library</p>
 
     <a class="ro-button ro-button-primary px-8 py-2 rounded-full text-lg font-bold" href="/docs">
